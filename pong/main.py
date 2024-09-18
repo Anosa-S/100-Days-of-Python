ca7752b07bf130e4ddc6ba2bdfb6782b6125e2bb
Anosa Sisane <anosa.sisane@gmail.com>
2024-09-17 20:33:34 -0400

    edited project (created ball class code)

--- pong/main.py
+++ pong/main.py
@@ -1,16 +1,18 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen =  Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("PONG")
-screen.tracer(1)
+screen.tracer(0)
 
 
 right_paddle = Paddle(350, 0)
 left_paddle = Paddle(-350, 0)
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.up, "Up")
@@ -18,4 +20,13 @@ screen.onkey(right_paddle.down, "Down")
 screen.onkey(left_paddle.up2, "w")
 screen.onkey(left_paddle.down2, "s")
 
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        ball.bounce()
+    
 screen.exitonclick()
